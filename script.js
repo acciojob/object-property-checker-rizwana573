663eaee18927fd388b796b80c8ab7b9e2dfe659b
rizwana573 <95849132+rizwana573@users.noreply.github.com>
2023-10-02 23:51:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-}
-
+	const keys = Object.keys(sampleObject);
+	//for(let key in keys){
+		if(!(key in sampleObject)){
+			return false;
+		} 
+	//}  
+	return true;
+}   
+  
 // Do not change the code below
+
 const key = prompt("Enter Key.");
 alert(hasKey(key));
